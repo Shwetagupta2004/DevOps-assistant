3373385abf66ed0a949ea0820536b33d4ca57673
Shweta Gupta <shweta.gupta@eqw.io>
2025-04-23 15:29:00 +0530

    frontend code updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // src/utils/api.js
 export const sendMessage = async (messages) => {
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch(`${API_BASE_URL}/api/chat`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
